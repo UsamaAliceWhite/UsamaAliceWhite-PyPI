263b6445e525f527754169cd5ca63a25eac93e1c
UsamaAliceWhite <usama.alice.white@gmail.com>
2026-02-21 06:21:02 +0900

    Add files via upload

---
+++ UsamaAliceWhite/Core/Pattern.py
@@ -0,0 +1,20 @@
+# Copyright 2026 UsamaAliceWhite All Rights Reserved
+
+
+# 標準モジュール
+import threading
+import typing
+
+
+# --- シングルトンパターン ---
+class SingletonPattern(type):
+    _instances: dict[type, typing.Any] = {}
+    _lock: threading.Lock = threading.Lock()
+
+    def __call__(cls, *args: typing.Any, **kwargs: typing.Any) -> typing.Any:
+        if cls not in cls._instances:
+            with cls._lock:
+                if cls not in cls._instances:
+                    instance: typing.Any = super().__call__(*args, **kwargs)
+                    cls._instances[cls] = instance
+        return cls._instances[cls]
